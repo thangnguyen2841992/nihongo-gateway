ef8e5f9b0d0682e3df400bba17d7ef353b1bb5ec
thangnguyen2841992 <ntthang4@cmcglobal.vn>
2026-03-26 22:33:27 +0700

    ll

--- src/main/java/com/nihongo/gateway/GatewayApplication.java
+++ src/main/java/com/nihongo/gateway/GatewayApplication.java
@@ -3,6 +3,11 @@ package com.nihongo.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +15,11 @@ public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public KeyResolver userKeyResolver() {
+		return exchange -> Mono.just(Objects.requireNonNull(exchange.getRequest().getRemoteAddress()).getAddress().getHostAddress());
 	}
 
 }
